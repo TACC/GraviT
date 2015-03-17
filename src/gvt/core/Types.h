7d1a7aeee30c2e89dadc7b1e8d33a7782c1d6945
Paul Navrátil <pnav@tacc.utexas.edu>
2015-03-17 18:06:21 -0500

    fixed indent

--- src/gvt/core/Types.h
+++ src/gvt/core/Types.h
@@ -13,22 +13,22 @@
 namespace gvt {
 	namespace core {
 		typedef std::string String;
-        typedef boost::uuids::uuid Uuid;
-        typedef boost::variant<int,long,float,double,bool,String,Uuid> Variant;
-        template <class T> using Vector = boost::container::vector<T>;
-        template <class K, class V> using Map = boost::container::map<K, V>;
+                typedef boost::uuids::uuid Uuid;
+                typedef boost::variant<int,long,float,double,bool,String,Uuid> Variant;
+                template <class T> using Vector = boost::container::vector<T>;
+                template <class K, class V> using Map = boost::container::map<K, V>;
 
-        inline Uuid nil_uuid() { return boost::uuids::nil_uuid(); }
-        inline Uuid make_uuid() { return Uuid(boost::uuids::random_generator()()); }
-        inline String uuid_toString(const Uuid& u) { return boost::uuids::to_string(u); }
+                inline Uuid nil_uuid() { return boost::uuids::nil_uuid(); }
+                inline Uuid make_uuid() { return Uuid(boost::uuids::random_generator()()); }
+                inline String uuid_toString(const Uuid& u) { return boost::uuids::to_string(u); }
 
-        inline int variant_toInteger(Variant v) { return boost::get<int>(v); }
-        inline long variant_toLong(Variant v) { return boost::get<long>(v); }
-        inline float variant_toFloat(Variant v) { return boost::get<float>(v); }
-        inline double variant_toDouble(Variant v) { return boost::get<double>(v); }
-        inline bool variant_toBoolean(Variant v) { return boost::get<bool>(v); }
-        inline String variant_toString(Variant v) { return boost::get<String>(v); }
-        inline Uuid variant_toUuid(Variant v) { return boost::get<Uuid>(v); }
+                inline int variant_toInteger(Variant v) { return boost::get<int>(v); }
+                inline long variant_toLong(Variant v) { return boost::get<long>(v); }
+                inline float variant_toFloat(Variant v) { return boost::get<float>(v); }
+                inline double variant_toDouble(Variant v) { return boost::get<double>(v); }
+                inline bool variant_toBoolean(Variant v) { return boost::get<bool>(v); }
+                inline String variant_toString(Variant v) { return boost::get<String>(v); }
+                inline Uuid variant_toUuid(Variant v) { return boost::get<Uuid>(v); }
 	}
 }
 
