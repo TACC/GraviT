0ff472421ba074671c6ada45558db3ed57c11547
rri <rribeiro@tacc.utexas.edu>
2016-01-08 01:52:47 +0000

    Added bbox rendering

--- src/apps/render/GLTrace.cpp
+++ src/apps/render/GLTrace.cpp
@@ -67,8 +67,11 @@
 #endif
 
 
+typedef enum {BVH_RENDER_MODE, FILM_RENDER_MODE} render_mode;
 
 #define ESCAPE 27
+#define ROTATE_STEP .1
+#define MOVE_STEP 0.1
 
 using namespace std;
 using namespace gvtapps::render;
@@ -84,14 +87,19 @@ using namespace gvt::render::data::primitives;
 // global variables used by glut callbacks.
 //
 GLubyte *imagebuffer;
-static GLint width, height;
-static GLint Height;
+static GLint width;
+static GLint height;
 Image *imageptr;
 int master;
 bool update = false;
+render_mode renderMode = BVH_RENDER_MODE;
+gvt::core::DBNodeH camNode;
 
-// ************************* ray tracing functions
-// **********************************************
+static int mouseButton0 = 0;
+static int mouseButton2 = 0;
+static int mouseGrabLastX = 0;
+static int mouseGrabLastY = 0;
+static double lastMouseUpdate = 0.0;
 
 inline double WallClockTime() {
 #if defined(__linux__) || defined(__APPLE__) || defined(__CYGWIN__) || defined(__OpenBSD__) || defined(__FreeBSD__)
@@ -106,8 +114,221 @@ inline double WallClockTime() {
 #endif
 }
 
-void Render() {
+void Translate(Point4f& eye, Point4f& focus,const Point4f &t) {
+  eye += t;
+  focus += t;
+}
 
+void  TranslateLeft(Point4f& eye, Point4f& focus, const float k) {
+  Point4f t = Point4f(-k, 0, 0,0);
+  Translate(eye,focus, t);
+}
+
+void  TranslateRight(Point4f& eye, Point4f& focus, const float k) {
+  Point4f t = Point4f(k, 0, 0,0);
+  Translate(eye,focus, t);
+}
+
+void  TranslateForward(Point4f& eye, Point4f& focus, const float k) {
+  Point4f t = k *(focus - eye);
+  Translate(eye,focus, t);
+}
+
+void  TranslateBackward(Point4f& eye, Point4f& focus, const float k) {
+  Point4f t = -k *(focus - eye);
+  Translate(eye,focus, t);
+}
+
+void Rotate(Point4f& eye, Point4f& focus,
+            const float angle, const Point4f& axis) {
+
+  Vector4f p = focus - eye;
+
+  gvt::core::math::Vector4f t = angle * axis;
+
+  gvt::core::math::AffineTransformMatrix<float> mAA;
+  mAA = gvt::core::math::AffineTransformMatrix<float>::createRotation(
+        t[0], 1.0, 0.0, 0.0) *
+      gvt::core::math::AffineTransformMatrix<float>::createRotation(
+        t[1], 0.0, 1.0, 0.0) *
+      gvt::core::math::AffineTransformMatrix<float>::createRotation(
+        t[2], 0.0,  0.0, 1.0);
+
+  gvt::core::math::AffineTransformMatrix<float> id;
+
+  // Rotate focus point
+  focus = eye + mAA*p;
+}
+
+void RotateLeft(Point4f& eye, Point4f& focus, float angle) {
+  Rotate( eye, focus,angle, Point4f(0, 1, 0,0));
+}
+
+void RotateRight(Point4f& eye, Point4f& focus, float angle) {
+  Rotate( eye, focus,-angle, Point4f(0, 1, 0,0));
+}
+
+void RotateUp(Point4f& eye, Point4f& focus, float angle) {
+  Rotate( eye, focus, angle, Point4f(1, 0, 0,0));
+}
+
+void RotateDown(Point4f& eye, Point4f& focus, float angle) {
+  Rotate( eye, focus, -angle, Point4f(1, 0, 0,0));
+}
+
+void UpdateCamera(Point4f focus, Point4f eye1)
+{
+  Point4f eye_o = gvt::core::variant_toPoint4f(camNode["eyePoint"].value());
+  Point4f focus_o = gvt::core::variant_toPoint4f(camNode["focus"].value());
+
+  cout << "old eye : " << eye_o[0] << " "<< eye_o[1] << " "<< eye_o[2] << endl;
+  cout << "new eye : " << eye1[0] << " "<< eye1[1] << " "<< eye1[2] << endl;
+  cout << "old focus : " << focus_o[0] << " "<< focus_o[1] << " "<< focus_o[2] << endl;
+  cout << "new focus : " << focus[0] << " "<< focus[1] << " "<< focus[2] << endl;
+
+  camNode["eyePoint"] = eye1;
+  camNode["focus"] = focus;
+
+  if (renderMode == BVH_RENDER_MODE){
+
+      Point4f up = gvt::core::variant_toVector4f(camNode["upVector"].value());
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+      gluLookAt(eye1[0],eye1[1],eye1[2],
+          focus[0],focus[1],focus[2],
+          up[0],up[1],up[2]);
+    }
+}
+
+
+static void mouseFunc(int button, int state, int x, int y) {
+  if (button == 0) {
+      if (state == GLUT_DOWN) {
+          // Record start position
+          mouseGrabLastX = x;
+          mouseGrabLastY = y;
+          mouseButton0 = 1;
+        } else if (state == GLUT_UP) {
+          mouseButton0 = 0;
+        }
+    } else if (button == 2) {
+      if (state == GLUT_DOWN) {
+          // Record start position
+          mouseGrabLastX = x;
+          mouseGrabLastY = y;
+          mouseButton2 = 1;
+        } else if (state == GLUT_UP) {
+          mouseButton2 = 0;
+        }
+    }
+}
+
+static void motionFunc(int x, int y) {
+
+  Point4f eye1 = gvt::core::variant_toPoint4f(camNode["eyePoint"].value());
+  Point4f focus = gvt::core::variant_toPoint4f(camNode["focus"].value());
+
+  const double minInterval = 0.2;
+  if (mouseButton0) {
+      // Check elapsed time since last update
+      if (WallClockTime() - lastMouseUpdate > minInterval) {
+          const int distX = x - mouseGrabLastX;
+          const int distY = y - mouseGrabLastY;
+
+          RotateDown(eye1,  focus, 0.004f * distY * ROTATE_STEP) ;
+          RotateRight(eye1,  focus, 0.004f * distX * ROTATE_STEP) ;
+
+          mouseGrabLastX = x;
+          mouseGrabLastY = y;
+
+          update = true;
+
+          UpdateCamera(focus, eye1);
+
+          glutPostRedisplay();
+          lastMouseUpdate = WallClockTime();
+        }
+    }
+}
+
+void reshape(int w, int h) {
+
+  gvt::render::RenderContext *cntxt = gvt::render::RenderContext::instance();
+  gvt::core::DBNodeH rootNode = cntxt->getRootNode();
+  gvt::core::DBNodeH filmNode = rootNode["Film"];
+
+  width=w;
+  height=h;
+  filmNode["width"]=w;
+  filmNode["height"]=h;
+
+  update = true;
+
+  glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+
+  if (renderMode == FILM_RENDER_MODE){
+
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+    }
+  else{
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      gluPerspective(45,(double)w/(double)h,.1,100);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+      Point4f eye1 = gvt::core::variant_toPoint4f(camNode["eyePoint"].value());
+      Point4f focus = gvt::core::variant_toPoint4f(camNode["focus"].value());
+      Point4f up = gvt::core::variant_toVector4f(camNode["upVector"].value());
+
+      gluLookAt(eye1[0],eye1[1],eye1[2],
+          focus[0],focus[1],focus[2],
+          up[0],up[1],up[2]);
+    }
+}
+
+void drawWireBox(gvt::render::data::primitives::Box3D& bbox ) {
+  float xmin= bbox.bounds[0][0];
+  float ymin= bbox.bounds[0][1];
+  float zmin= bbox.bounds[0][2];
+  float xmax= bbox.bounds[1][0];
+  float ymax= bbox.bounds[1][1];
+  float zmax= bbox.bounds[1][2];
+
+  glPushMatrix();
+  glTranslatef(0.5f*(xmin + xmax), 0.5f*(ymin + ymax), 0.5f*(zmin + zmax));
+  glScalef(xmax - xmin, ymax - ymin, zmax - zmin);
+  glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+  glutWireCube(1.0f);
+  glPopMatrix();
+}
+
+void RenderBVH(){
+
+  gvt::render::RenderContext *cntxt = gvt::render::RenderContext::instance();
+  gvt::core::DBNodeH rootNode = cntxt->getRootNode();
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
+
+  for ( gvt::core::DBNodeH instance : rootNode["Instances"].getChildren()){
+      Box3D *bbox = gvt::core::variant_toBox3DPtr(instance["bbox"].value());
+      drawWireBox(*bbox);
+    }
+
+  //glutSolidTeapot(.5);
+
+  glFlush();
+
+}
+
+void Render() {
   gvt::render::RenderContext *cntxt = gvt::render::RenderContext::instance();
   gvt::core::DBNodeH rootNode = cntxt->getRootNode();
   gvt::core::DBNodeH camNode = rootNode["Camera"];
@@ -153,164 +374,44 @@ void Render() {
 
 }
 
-//// ************************* Glut callback  functions
-//// ********************************************
-
-
-void Translate(Point4f& eye, Point4f& focus,const Point4f &t) {
-  eye += t;
-  focus += t;
-}
-
-void  TranslateLeft(Point4f& eye, Point4f& focus, const float k) {
-  Point4f t = Point4f(-k, 0, 0,0);
-  Translate(eye,focus, t);
-}
-
-
-void  TranslateRight(Point4f& eye, Point4f& focus, const float k) {
-  Point4f t = Point4f(k, 0, 0,0);
-  Translate(eye,focus, t);
-}
-
-void  TranslateForward(Point4f& eye, Point4f& focus, const float k) {
-  Point4f t = k *(focus - eye);
-  Translate(eye,focus, t);
-}
-
-void  TranslateBackward(Point4f& eye, Point4f& focus, const float k) {
-  Point4f t = -k *(focus - eye);
-  Translate(eye,focus, t);
-}
-
-
-void Rotate(Point4f& eye, Point4f& focus,
-            const float angle, const Point4f& axis) {
-
-  Vector4f p = focus - eye;
-
-  gvt::core::math::AffineTransformMatrix<float> m;
-  gvt::core::math::Vector4f t = angle * axis;
-
-  m = gvt::core::math::AffineTransformMatrix<float>::createRotation(
-        t[0], 1.0, 0.0, 0.0) *
-      gvt::core::math::AffineTransformMatrix<float>::createRotation(
-        t[1], 0.0, 1.0, 0.0) *
-      gvt::core::math::AffineTransformMatrix<float>::createRotation(
-        t[2], 0.0,  0.0, 1.0);
-
-  //focus = eye + m*p;
-  eye = eye*m;
-}
-
-void RotateLeft(Point4f& eye, Point4f& focus,const float angle) {
-  Rotate( eye, focus,-angle, Point4f(0, 1, 0,0));
-}
-
-void RotateRight(Point4f& eye, Point4f& focus,const float angle) {
-  Rotate( eye, focus,angle, Point4f(0, 1, 0,0));
-}
-
-void RotateUp(Point4f& eye, Point4f& focus,const float angle) {
-  Rotate( eye, focus, angle, Point4f(1, 0, 0,0));
-}
-
-void RotateDown(Point4f& eye, Point4f& focus,const float angle) {
-  Rotate( eye, focus, -angle, Point4f(1, 0, 0,0));
-}
-
-static int mouseButton0 = 0;
-static int mouseButton2 = 0;
-static int mouseGrabLastX = 0;
-static int mouseGrabLastY = 0;
-static double lastMouseUpdate = 0.0;
-
-static void mouseFunc(int button, int state, int x, int y) {
-  if (button == 0) {
-      if (state == GLUT_DOWN) {
-          // Record start position
-          mouseGrabLastX = x;
-          mouseGrabLastY = y;
-          mouseButton0 = 1;
-        } else if (state == GLUT_UP) {
-          mouseButton0 = 0;
-        }
-    } else if (button == 2) {
-      if (state == GLUT_DOWN) {
-          // Record start position
-          mouseGrabLastX = x;
-          mouseGrabLastY = y;
-          mouseButton2 = 1;
-        } else if (state == GLUT_UP) {
-          mouseButton2 = 0;
-        }
-    }
-}
-
-#define ROTATE_STEP 1
-#define MOVE_STEP 0.1
-static void motionFunc(int x, int y) {
-
-  gvt::render::RenderContext *cntxt = gvt::render::RenderContext::instance();
-  gvt::core::DBNodeH rootNode = cntxt->getRootNode();
-  gvt::core::DBNodeH camNode = rootNode["Camera"];
-  Point4f eye1 = gvt::core::variant_toPoint4f(camNode["eyePoint"].value());
-  Point4f focus = gvt::core::variant_toPoint4f(camNode["focus"].value());
-
-  const double minInterval = 0.2;
-  if (mouseButton0) {
-      // Check elapsed time since last update
-      if (WallClockTime() - lastMouseUpdate > minInterval) {
-          const int distX = x - mouseGrabLastX;
-          const int distY = y - mouseGrabLastY;
-
-          RotateDown(eye1,  focus, 0.04f * distY * ROTATE_STEP) ;
-          RotateRight(eye1,  focus, 0.04f * distX * ROTATE_STEP) ;
-
-          mouseGrabLastX = x;
-          mouseGrabLastY = y;
-
-          update = true;
-
-          camNode["eyePoint"] = eye1;
-          camNode["focus"] = focus;
-
-          glutPostRedisplay();
-          lastMouseUpdate = WallClockTime();
-        }
-    }
-}
-
-void reshape(int w, int h) {
-  glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-  Height = (GLint)h;
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-}
-
-void dispfunc(void) {
+void RenderFilm(){
   unsigned char key = 'r';
   if (update) {
       MPI_Bcast(&key, 1, MPI_UNSIGNED_CHAR, master, MPI_COMM_WORLD);
       Render();
       update = false;
     }
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glRasterPos2i(0, 0);
   glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, imagebuffer);
   glFlush();
+
 }
 
+void dispfunc(void) {
 
+
+  switch (renderMode) {
+    case BVH_RENDER_MODE:
+      {
+        RenderBVH();
+        break;
+      }
+    case FILM_RENDER_MODE:
+      {
+        RenderFilm();
+        break;
+      }
+    default:
+      {
+        cout << "unknown render mode" << endl;
+        break;
+      }
+    }
+}
 
 void specialkey(int key, int x, int y) {
 
-  gvt::render::RenderContext *cntxt = gvt::render::RenderContext::instance();
-  gvt::core::DBNodeH rootNode = cntxt->getRootNode();
-  gvt::core::DBNodeH camNode = rootNode["Camera"];
   Point4f eye1 = gvt::core::variant_toPoint4f(camNode["eyePoint"].value());
   Point4f focus = gvt::core::variant_toPoint4f(camNode["focus"].value());
 
@@ -336,16 +437,43 @@ void specialkey(int key, int x, int y) {
       break;
     }
 
-  camNode["eyePoint"] = eye1;
-  camNode["focus"] = focus;
+  UpdateCamera(focus, eye1);
 
   glutPostRedisplay();
 }
 
+void UpdateRenderMode()
+{
+  if (renderMode == BVH_RENDER_MODE){
+      renderMode = FILM_RENDER_MODE;
+
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      gluOrtho2D(0.0, (GLdouble)width, 0.0, (GLdouble)height);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+    }
+  else{
+      renderMode = BVH_RENDER_MODE;
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      gluPerspective(45,(double)width/(double)height,.1,100);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+      Point4f eye1 = gvt::core::variant_toPoint4f(camNode["eyePoint"].value());
+      Point4f focus = gvt::core::variant_toPoint4f(camNode["focus"].value());
+      Point4f up = gvt::core::variant_toVector4f(camNode["upVector"].value());
+
+      gluLookAt(eye1[0],eye1[1],eye1[2],
+          focus[0],focus[1],focus[2],
+          up[0],up[1],up[2]);
+    }
+}
+
 void keyboard(unsigned char key, int x, int y) {
-  gvt::render::RenderContext *cntxt = gvt::render::RenderContext::instance();
-  gvt::core::DBNodeH rootNode = cntxt->getRootNode();
-  gvt::core::DBNodeH camNode = rootNode["Camera"];
+
   Point4f eye1 = gvt::core::variant_toPoint4f(camNode["eyePoint"].value());
   Point4f focus = gvt::core::variant_toPoint4f(camNode["focus"].value());
 
@@ -355,6 +483,7 @@ void keyboard(unsigned char key, int x, int y) {
       if (MPI::COMM_WORLD.Get_size() > 1)
         MPI_Finalize();
       exit(0);
+
     case 'q':
       MPI_Bcast(&key, 1, MPI_UNSIGNED_CHAR, master, MPI_COMM_WORLD);
       if (MPI::COMM_WORLD.Get_size() > 1)
@@ -362,11 +491,11 @@ void keyboard(unsigned char key, int x, int y) {
       exit(0);
 
     case 'w':
-      RotateDown(eye1,  focus,  ROTATE_STEP) ;
+      RotateUp(eye1,  focus,  ROTATE_STEP) ;
       update = true;
       break;
     case 's':
-      RotateUp(eye1,  focus,  ROTATE_STEP) ;
+      RotateDown(eye1,  focus,  ROTATE_STEP) ;
       update = true;
       break;
     case 'a':
@@ -378,12 +507,16 @@ void keyboard(unsigned char key, int x, int y) {
       update = true;
       break;
 
+    case 'm':
+      UpdateRenderMode();
+      break;
     default:
       // dont do anything
       break;
     }
-  camNode["eyePoint"] = eye1;
-  camNode["focus"] = focus;
+
+  if (update)
+    UpdateCamera(focus, eye1);
 
   glutPostRedisplay();
 }
@@ -393,9 +526,7 @@ void ConfigSceneFromFile(string filename){
   gvtapps::render::ConfigFileLoader cl(filename);
   gvt::render::data::Dataset& scene = cl.scene;
 
-
   gvt::render::RenderContext *cntxt = gvt::render::RenderContext::instance();
-
 
   gvt::core::DBNodeH root = cntxt->getRootNode();
   gvt::core::DBNodeH dataNodes = cntxt->createNodeFromType("Data","Data",root.UUID());
@@ -447,11 +578,11 @@ void ConfigSceneFromFile(string filename){
   lightNode["color"] = Vector4f(lp->color);
 
   // camera
-  gvt::core::DBNodeH camNode = cntxt->createNodeFromType("Camera","Camera",root.UUID());
-  camNode["eyePoint"] = Point4f(scene.camera.getEye());
-  camNode["focus"] = Point4f(scene.camera.getFocus());
-  camNode["upVector"] = scene.camera.getUp();
-  camNode["fov"] = (float)(25.0 * M_PI/180.0); // TODO
+  gvt::core::DBNodeH _camNode = cntxt->createNodeFromType("Camera","Camera",root.UUID());
+  _camNode["eyePoint"] = Point4f(scene.camera.getEye());
+  _camNode["focus"] = Point4f(scene.camera.getFocus());
+  _camNode["upVector"] = scene.camera.getUp();
+  _camNode["fov"] = (float)(45.0 * M_PI/180.0); // TODO
 
   // film
   gvt::core::DBNodeH filmNode = cntxt->createNodeFromType("Film","Film",root.UUID());
@@ -620,12 +751,12 @@ void ConfigSceneCubeCone(){
   //  lN2["position"] = Vector4f(2.0, 2.0, 2.0, 0.0);
   //  lN2["color"] = Vector4f(0.0, 0.0, 0.0, 0.0);
 
-  gvt::core::DBNodeH camNode =
+  gvt::core::DBNodeH _camNode =
       cntxt->createNodeFromType("Camera", "Camera", root.UUID());
-  camNode["eyePoint"] = Point4f(4.0, 0.0, 0.0, 1.0);
-  camNode["focus"] = Point4f(0.0, 0.0, 0.0, 1.0);
-  camNode["upVector"] = Vector4f(0.0, 1.0, 0.0, 0.0);
-  camNode["fov"] = (float)(45.0 * M_PI / 180.0);
+  _camNode["eyePoint"] = Point4f(4.0, 0.0, 0.0, 1.0);
+  _camNode["focus"] = Point4f(0.0, 0.0, 0.0, 1.0);
+  _camNode["upVector"] = Vector4f(0.0, 1.0, 0.0, 0.0);
+  _camNode["fov"] = (float)(45.0 * M_PI / 180.0);
 
   gvt::core::DBNodeH filmNode =
       cntxt->createNodeFromType("Film", "Film", root.UUID());
@@ -633,8 +764,6 @@ void ConfigSceneCubeCone(){
   filmNode["height"] = 512;
 }
 
-// ******************************* main
-// ****************************************************
 int main(int argc, char *argv[]) {
   unsigned char action;
   // mpi initialization
@@ -663,8 +792,8 @@ int main(int argc, char *argv[]) {
       exit(0);
     }
 
-  //ConfigSceneFromFile(filename);
-  ConfigSceneCubeCone();
+  ConfigSceneFromFile(filename);
+  //ConfigSceneCubeCone();
 
   gvt::core::DBNodeH root = cntxt->getRootNode();
 
@@ -687,8 +816,8 @@ int main(int argc, char *argv[]) {
 
   schedNode["adapter"] = adapterType;
 
+  camNode = root["Camera"];
   width =  gvt::core::variant_toInteger(root["Film"]["width"].value());
-
   height=  gvt::core::variant_toInteger(root["Film"]["height"].value());
 
   Image image(width,
@@ -699,25 +828,52 @@ int main(int argc, char *argv[]) {
 
   Render();
 
-  //
-  // Opengl display stuff goes here
-  //
-
   if (rank == master) { // max rank process does display
+
       glutInit(&argc, argv);
-      glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+      glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
       glutInitWindowSize(width, height);
       glutInitWindowPosition(10, 10);
       glutCreateWindow(filename.c_str());
+
       glClearColor(0., 0., 0., 0.);
-      glShadeModel(GL_FLAT);
-      glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
       glutDisplayFunc(dispfunc);
       glutSpecialFunc(specialkey);
       glutKeyboardFunc(keyboard);
       glutReshapeFunc(reshape);
       glutMouseFunc(mouseFunc);
       glutMotionFunc(motionFunc);
+
+      /* set up depth-buffering */
+      glEnable(GL_DEPTH_TEST);
+
+      if (renderMode == FILM_RENDER_MODE){
+
+          glMatrixMode(GL_PROJECTION);
+          glLoadIdentity();
+          gluOrtho2D(0.0, (GLdouble)width, 0.0, (GLdouble)height);
+          glMatrixMode(GL_MODELVIEW);
+          glLoadIdentity();
+
+        }
+      else{
+          glMatrixMode(GL_PROJECTION);
+          glLoadIdentity();
+          gluPerspective(45,(double)width/(double)height,.1,100);
+          glMatrixMode(GL_MODELVIEW);
+          glLoadIdentity();
+
+          Point4f eye1 = gvt::core::variant_toPoint4f(camNode["eyePoint"].value());
+          Point4f focus = gvt::core::variant_toPoint4f(camNode["focus"].value());
+          Point4f up = gvt::core::variant_toVector4f(camNode["upVector"].value());
+
+          gluLookAt(eye1[0],eye1[1],eye1[2],
+              focus[0],focus[1],focus[2],
+              up[0],up[1],up[2]);
+        }
+
       glutMainLoop();
     } else { // loop and wait for further instructions
       while (1) {
