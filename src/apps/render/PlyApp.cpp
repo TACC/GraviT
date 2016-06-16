354dcbfa2011d43f59eb8a6b53492cb3a1283778
Roberto Ribeiro <rribeiro@tacc.utexas.edu>
2016-06-16 18:12:58 -0500

    Fix on gvtPly to load arbitrairly .ply files

--- src/apps/render/PlyApp.cpp
+++ src/apps/render/PlyApp.cpp
@@ -206,13 +206,13 @@ int main(int argc, char **argv) {
   vector<string>::const_iterator file;
   //for (k = 0; k < 8; k++) {
   for (file = files.begin(),k = 0; file != files.end(); file++, k++) {
-    sprintf(txt, "%d", k);
-    filename = "block";
-    filename += txt;
-    gvt::core::DBNodeH EnzoMeshNode = cntxt->createNodeFromType("Mesh", filename.c_str(), dataNodes.UUID());
+    //sprintf(txt, "%d", k);
+    //filename = "block";
+    //filename += txt;
+    gvt::core::DBNodeH EnzoMeshNode = cntxt->createNodeFromType("Mesh",*file, dataNodes.UUID());
     // read in some ply data and get ready to load it into the mesh
     // filepath = rootdir + "block" + std::string(txt) + ".ply";
-    filepath = rootdir + "/" + filename + ".ply";
+    filepath =  *file;
     myfile = fopen(filepath.c_str(), "r");
     in_ply = read_ply(myfile);
     for (i = 0; i < in_ply->num_elem_types; i++) {
